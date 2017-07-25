5de0e8ae6037b41dd335335277cabd68892d9498
Miro Dojkic <mdojkic@gmail.com>
2017-07-25 09:09:44 +0200

    FIX: Display invitation only courses in explore page to global staff (#257)
    * update ccx filters to take user into account
    
    * display invitation only course for global staff

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -152,6 +152,8 @@ def courses(request):
 
     if should_hide_master_course:
         courses = CourseOverview.objects.filter(id__in=ccx_keys)
+    elif request.user.is_staff:
+        courses = CourseOverview.objects.filter(Q(id__in=ccx_keys) | ~Q(id__startswith='ccx'))
     else:
         courses = CourseOverview.objects.filter(Q(id__in=ccx_keys) | ~Q(id__startswith='ccx')).filter(invitation_only=0)
 
