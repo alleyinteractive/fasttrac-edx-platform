45c94c75b0130df990f93c78390bfdd450d66526
mbareta <mbareta@extensionengine.com>
2017-11-10 10:39:16 +0100

    Send Mandrill email template to students in list (#389)
    * Send Mandrill email template to students in list
    
    * Add new logic for sending emails

--- lms/djangoapps/ccx/tasks.py
+++ lms/djangoapps/ccx/tasks.py
@@ -2,18 +2,22 @@
 Asynchronous tasks for the CCX app.
 """
 
-import datetime
+from datetime import datetime, timedelta, date, time
 from django.dispatch import receiver
 import logging
-
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.locator import CourseLocator
 from ccx_keys.locator import CCXLocator
 from xmodule.modulestore.django import SignalHandler
 from lms import CELERY_APP
 from courseware.models import StudentModule
-
+import requests
+from django.conf import settings
 from lms.djangoapps.ccx.models import CustomCourseForEdX
+from django.contrib.auth.models import User
+from student.models import CourseEnrollment, CourseAccessRole
+from opaque_keys.edx.keys import CourseKey
+
 
 log = logging.getLogger("edx.ccx")
 
@@ -49,18 +53,65 @@ def send_ccx_course_published(course_key):
 
 @CELERY_APP.task(name='ccx.print')
 def send_emails():
-    print("TEST Send emails....!!!")
+    log.info('Sending Mandrill/Mailchimp post-course emails...')
 
-    # One day after the "course completion date" for all learners enrolled in that course (not affiliates); OR
-    # today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
-    # today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
-    # ccxs = CustomCourseForEdX.objects.filter(end_date__range=(today_min, today_max))
+    mandrill_url = 'https://mandrillapp.com/api/1.0/messages/send-template.json'
+    mandrill_api_key = settings.MANDRILL_API_KEY
 
-    # ccxs = CustomCourseForEdX.objects.all()
-    # learner_enrollments = [enrollment for enrollment in ccx.students for ccx in ccxs]
+    if mandrill_api_key is None:
+        return
 
-    # print(len(learner_enrollments))
+    today = date.today()
 
-    # for enrollment in learner_enrollments:
-    #     print(enrollment)
-    # 45 days after a user's last activity in a course in which he/she is still enrolled
+    # One day after the "course completion date" for all learners enrolled in that course (not affiliates)
+    yesterday = today - timedelta(days=1)
+
+    ccx_min_date = datetime.combine(yesterday, time.min)
+    ccx_max_date = datetime.combine(yesterday, time.max)
+
+    ccxs = CustomCourseForEdX.objects.filter(end_date__range=(ccx_min_date, ccx_max_date))
+    students = [enrollment.user for enrollment in ccx.students for ccx in ccxs]
+
+    student_emails = [{'email': student.email, 'name': student.profile.name, 'type': 'to'} for student in students]
+
+    # 45 days after a user's last activity in a course in which they are still enrolled
+    n_days = 45
+    n_days_ago = today - timedelta(days=n_days)
+
+    users_min_date = datetime.combine(n_days_ago, time.min)
+    users_max_date = datetime.combine(n_days_ago, time.max)
+
+    users = User.objects.filter(last_login__range=(users_min_date, users_max_date))
+
+    for user in users:
+        course_key = CourseKey.from_string(settings.FASTTRAC_COURSE_KEY)
+        is_enrolled_in_ccx = CourseEnrollment.objects.filter(user=user).exclude(course_id=course_key).exists()
+        is_affiliate_staff = CourseAccessRole.objects.filter(user=user).exists()
+
+        if not is_enrolled_in_ccx and not is_affiliate_staff:
+            # send post-course email if student is not enrolled in a ccx
+            student_emails.append({
+                'email': user.email,
+                'name': user.profile.name,
+                'type': 'to'
+            })
+
+    data = {
+        "key": mandrill_api_key,
+        "template_name": "fasttrac-post-course-survey",
+        "template_content": {},
+        "message": {
+            "to": student_emails,
+            "from_email": "info@fasttrac.org",
+            "from_name": "Fasttrac",
+            "signing_domain": "fasttrac.org",
+            "subject": "You completed a FastTrac course"
+        }
+    }
+
+    request = requests.post(mandrill_url, json=data)
+    if not request.status_code == 200:
+        print('Mandrill API send error')
+        print(request.content)
+    else:
+        print('Mandrill API send success')
