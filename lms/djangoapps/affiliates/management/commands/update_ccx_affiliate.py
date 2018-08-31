9ae643f77b1aa4229c9bc5ecf04d30ac45fc1370
Vedran Karacic <karacicvedran@gmail.com>
2018-08-31 11:49:02 +0200

    Fix linting issues in management commands.

--- lms/djangoapps/affiliates/management/commands/update_ccx_affiliate.py
+++ lms/djangoapps/affiliates/management/commands/update_ccx_affiliate.py
@@ -7,7 +7,7 @@ from lms.djangoapps.ccx.models import CustomCourseForEdX
 class Command(BaseCommand):
     help = "Command for updating CCX models with affiliate relationships."
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # pylint: disable=unused-argument
         for affiliate in AffiliateEntity.objects.all():
             self.stdout.write('Updating affiliate {}'.format(affiliate.name))
             CustomCourseForEdX.objects.filter(
