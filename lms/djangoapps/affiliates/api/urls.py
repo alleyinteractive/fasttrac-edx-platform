3c446be506024aeebd6430194ade1bfd31b9eeee
Vedran Karacic <karacicvedran@gmail.com>
2018-09-26 11:42:54 +0200

    Change order of affiliates API URL patterns.
    The previous order was capturing 'impersonate' and 'data-exports'
    with the '[^/]*' pattern as well.

--- lms/djangoapps/affiliates/api/urls.py
+++ lms/djangoapps/affiliates/api/urls.py
@@ -13,8 +13,8 @@ from affiliates.api.views import (
 urlpatterns = patterns(
     '',
     url(r'^$', AffiliateEntityViewSet.as_view(), name='create'),
-    url(r'^(?P<affiliate_slug>[^/]*)$', AffiliateEntityDetailsViewSet.as_view(), name='details'),
-    url(r'^(?P<affiliate_slug>[^/]*)/membership$', AffiliateEntityMembershipViewSet.as_view(), name='membership'),
     url(r'^impersonate$', ImpersonateView.as_view(), name='impersonate'),
-    url(r'^data-exports$', DataExportView.as_view(), name='data-exports')
+    url(r'^data-exports$', DataExportView.as_view(), name='data-exports'),
+    url(r'^(?P<affiliate_slug>[^/]+)$', AffiliateEntityDetailsViewSet.as_view(), name='details'),
+    url(r'^(?P<affiliate_slug>[^/]+)/membership$', AffiliateEntityMembershipViewSet.as_view(), name='membership'),
 )
